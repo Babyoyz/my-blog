e8261aa3123fa0627b075bce6e2deb643251b848
thaweepornoil <44115504+Babyoyz@users.noreply.github.com>
2023-09-05 21:27:28 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,15 @@ export default function Home() {
 return (
     <main className="">
         <div className='Corasel'>
-          asdasdasd
+        <div className="flex h-screen justify-center items-center">
+  <div className="flex flex-col">
+    <div className="flex-grow"></div>
+    <div className="flex-shrink text-center">
+      <p className="text-2xl">Coming Soon ...</p>
+    </div>
+    <div className="flex-grow"></div>
+  </div>
+</div>
 
         </div>
     </main>
