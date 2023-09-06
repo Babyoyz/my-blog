e94d5072c7d028877ae30f7f08cd2b2ecded0a04
Babyoyz <105766777+thaweepornoil@users.noreply.github.com>
2023-09-06 17:33:35 +0700

    add loadding api

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,11 @@ const MultipleCarousel = dynamic(
   () => import("@/components/MultipleCarousel"),
   {
     loading: () => (
-      <div className=" w-full h-screen  overflow-hiddenopacity-75 flex flex-col items-center justify-center">
+      <div className="w-full h-screen   opacity-75 flex flex-col items-center justify-center">
         <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+        <h2 className="text-center text-neutral-700 dark:text-white text-xl font-semibold">
+          Loading...
+        </h2>
       </div>
     ),
   }
@@ -54,7 +57,12 @@ export default function Home() {
               ))}
             </div>
           ) : (
-            <p>Loading...</p>
+            <div className="w-full h-screen   opacity-75 flex flex-col items-center justify-center">
+              <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+              <h2 className="text-center text-neutral-700 dark:text-white text-xl font-semibold">
+                Loading...
+              </h2>
+            </div>
           )}
         </div>
       </div>
